9efe721e8768f7d4ea90dfd8ab166f4b0f1ff56b
Jay Idrees <idreesjay@gmail.com>
2020-12-01 09:22:56 -0500

    Added event listener for self installation and adding files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,3 +12,12 @@ const FILES_TO_CACHE = [
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
+
+// Adding event listener for self installation and cacheing files
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+// Activate service worker automatically when the installation is complete
+  self.skipWaiting();
+});

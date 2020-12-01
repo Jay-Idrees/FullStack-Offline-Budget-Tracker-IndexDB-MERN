100bf10cac20508fd7c86208d624d717b226ab72
Jay Idrees <idreesjay@gmail.com>
2020-12-01 09:24:54 -0500

    Added eventlistener for deleting old files in the cache upon activation

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,3 +21,21 @@ self.addEventListener("install", function (evt) {
 // Activate service worker automatically when the installation is complete
   self.skipWaiting();
 });
+
+// Deleting old files in the cache on activation
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
